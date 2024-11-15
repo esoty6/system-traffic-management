a25ac05ce446028551342896a673aa71788f7be0
esoty6 <kamilwitek12@o2.pl>
2024-11-15 13:03:48 +0100

    move markup to new components

--- src/app/dashboard/dashboard-item/dashboard-item.component.ts
+++ src/app/dashboard/dashboard-item/dashboard-item.component.ts
@@ -1,12 +1,13 @@
-import { Component } from '@angular/core';
+import { Component, input } from '@angular/core';
 
 @Component({
   selector: 'app-dashboard-item',
   standalone: true,
   imports: [],
   templateUrl: './dashboard-item.component.html',
-  styleUrl: './dashboard-item.component.css'
+  styleUrl: './dashboard-item.component.css',
 })
 export class DashboardItemComponent {
-
+  readonly image = input.required<{ src: string; alt: string }>();
+  readonly title = input.required<string>();
 }
